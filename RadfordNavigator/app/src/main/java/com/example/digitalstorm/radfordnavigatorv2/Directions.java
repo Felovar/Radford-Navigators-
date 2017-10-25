ef7b11944204a0590ba7a6f310a9519d2645a634
jdearly <jearly10@radford.edu>
2017-10-25 15:13:48 -0400

    initial commit for project fils

---
+++ RadfordNavigator/app/src/main/java/com/example/digitalstorm/radfordnavigatorv2/Directions.java
@@ -0,0 +1,19 @@
+package com.example.digitalstorm.radfordnavigatorv2;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by Digital Storm on 10/24/2017.
+ */
+
+public class Directions extends Activity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.direction_screen);
+
+    }
+}
